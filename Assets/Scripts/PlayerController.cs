8291178cafad38a6faaed4ea7fbd5a37246a94c0
Sean Mansell <thearknano@gmail.com>
2017-01-21 06:06:23 +1030

    First pass player movement complete, implemented 'power' and boost

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using System.Collections;
-using InControl;
 
 public class PlayerController : MonoBehaviour {
 
@@ -9,32 +8,58 @@ public class PlayerController : MonoBehaviour {
     public float vertSpeed;
     public float turnSpeed;
     public float slowSpeed;
+    public float boostFactor;
+    public float maxBoostSpeed;
+    public float boostDrainSpeed;
 
-    InputDevice controller;
+    float currentForwardSpeed;
     Rigidbody rb;
     public Transform thruster;
+    PlayerPower power;
 
     // Use this for initialization
     void Start () {
-        controller = InputManager.ActiveDevice;
         rb = GetComponent<Rigidbody>();
+        currentForwardSpeed = forwardSpeed;
+        power = GetComponent<PlayerPower>();
     }
 	
+
+
 	// Update is called once per frame
 	void FixedUpdate () {
-        float horizontal = Input.GetAxis("Horizontal") + controller.LeftStick.X;
-        float vertical = Input.GetAxis("Vertical") + controller.LeftStick.Y;
-        float turn = Input.GetAxis("Turn") + controller.RightStick.X;
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        float turn = Input.GetAxis("Turn");
 
         Vector3 velocity = rb.velocity;
 
+        if (Input.GetButton("Boost"))
+        {
+            currentForwardSpeed = currentForwardSpeed + Time.deltaTime * boostFactor;
+            power.currentPower = power.currentPower - Time.deltaTime * boostDrainSpeed;
+            if (currentForwardSpeed >= maxBoostSpeed)
+            {
+                currentForwardSpeed = maxBoostSpeed;
+            }
+        } else
+        {
+            currentForwardSpeed = currentForwardSpeed - Time.deltaTime * (boostFactor / 2);
+            if (currentForwardSpeed <= forwardSpeed)
+            {
+                currentForwardSpeed = forwardSpeed;
+            }
+        }
+
         //rb.AddForce(transform.forward * 10)
-        transform.Translate(transform.forward * forwardSpeed, Space.World);
+        transform.Translate(transform.forward * currentForwardSpeed, Space.World);
         rb.AddForce(transform.right * horizontal * horizSpeed);
         rb.AddForce(transform.up * vertical * vertSpeed);
         rb.AddForce(-velocity * slowSpeed);
 
         rb.AddForceAtPosition(-transform.right * turn * turnSpeed, thruster.position);
         rb.angularVelocity = rb.angularVelocity * 0.9f;
+
+
     }
 }
---
+++ Assets/Scripts/PlayerPower.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerPower : MonoBehaviour {
+
+    public int startingPower;
+    public float currentPower;
+    public Slider powerSlider;
+    public float rechargeSpeed;
+
+	// Use this for initialization
+	void Start () {
+        currentPower = startingPower;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        currentPower = currentPower + Time.deltaTime * rechargeSpeed;
+        if (currentPower >= startingPower)
+        {
+            currentPower = startingPower;
+        }
+        powerSlider.value = currentPower;
+	}
+}
