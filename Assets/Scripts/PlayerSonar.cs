a0de498540e47f9933dc2ac9e9e9995181f386e2
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2017-01-21 15:19:26 +1030

    Combine sonar shader with Unity standard shader

--- Assets/Scripts/PlayerSonar.cs
+++ Assets/Scripts/PlayerSonar.cs
@@ -5,6 +5,8 @@ public class PlayerSonar : MonoBehaviour {
 
     public Shader shader;
     public float sonarSpeed;
+    public Color sonarColour;
+    public float sonarThickness;
 
     private const float INITIAL_SONAR_DISTANCE = -100;
     private float sonarDistance = INITIAL_SONAR_DISTANCE;
@@ -22,12 +24,18 @@ public class PlayerSonar : MonoBehaviour {
         }
         sonarDistance += Time.fixedDeltaTime * sonarSpeed;
 
-        Vector4 sonarPlane = getSonarPlane();
-        Shader.SetGlobalVector("_SonarPlane", sonarPlane);
+        setShaderValues(getSonarPlane(), sonarColour, sonarThickness);
 	}
 
     public Vector4 getSonarPlane()
     {
         return new Vector4(sonarDistance, 0, 0, 1);
+    }
+
+    private void setShaderValues(Vector4 plane, Vector4 colour, float thickness)
+    {
+        Shader.SetGlobalVector("_SonarPlane", plane);
+        Shader.SetGlobalVector("_SonarColour", colour);
+        Shader.SetGlobalFloat("_SonarThickness", thickness);
     }
 }
