ac5e392d75ff57b61f3896b9ca18a1cd2f85bdd7
Sean Mansell <thearknano@gmail.com>
2017-01-21 17:58:08 +1030

    Updated Player Controller, changed UI, added bubble sprite

---
+++ Assets/PreWave.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PreWave : MonoBehaviour {
+
+    public Transform player;
+    public float speed;
+    PlayerController pc;
+
+	// Use this for initialization
+	void Start () {
+        pc = player.gameObject.GetComponent<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime * pc.prewaveSpeedBoost);
+	}
+}
