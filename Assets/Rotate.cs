1c5afcc45de5ed7f90a32b55095d0a451cd14c7b
Sean Mansell <thearknano@gmail.com>
2017-01-22 03:57:53 +1030

    Added ring prefabs and pickups + score

---
+++ Assets/Rotate.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+    public float speed = 1;
+    public int value = 1;
+    PlayerController player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.up * Time.deltaTime * speed);
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            player.score = player.score + value;
+            Destroy(gameObject);
+        }
+    }
+}
