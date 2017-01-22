bf5a28890226f48a6b1a6824b862e7d18e8b52a7
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2017-01-22 13:32:48 +1030

    Set player collision damage based on angle of contact and speed

--- Assets/Scripts/PlayerDamage.cs
+++ Assets/Scripts/PlayerDamage.cs
@@ -7,8 +7,11 @@ public class PlayerDamage : MonoBehaviour {
     PlayerController player;
     float damage;
 
+    public float damageMultiplier;
     private float damageTime = 0.0f;
     public float damageInterval = 1.0f;
+
+    private bool damageEnabled = true;
 
     // Use this for initialization
     void Start () {
@@ -18,16 +21,39 @@ public class PlayerDamage : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        damage = player.currentForwardSpeed * player.prewaveSpeedBoost * 100;
+        //damage = player.currentForwardSpeed * player.prewaveSpeedBoost * 100;
         
 	}
 
-    void OnCollisionEnter (Collision collided)
+    void OnCollisionEnter (Collision collision)
     {
-        if (Time.time >= damageTime)
+        slideCollision(collision);
+        StartCoroutine(DisableDamage());
+    }
+
+    private void slideCollision(Collision collision)
+    {
+        float dotProduct = Vector3.Dot(-collision.contacts[0].normal, transform.forward);
+        float angle = 90 - (180 / Mathf.PI) * Mathf.Acos(dotProduct);
+        float pain = damageMultiplier * (angle / 90) * player.currentForwardSpeed;
+        if (damageEnabled)
+        {
+            damage -= pain;
+            health.TakeDamage(pain);
+        }
+
+        Debug.DrawRay(transform.position, transform.up * 20 * pain, Color.magenta);
+        /*if (Time.time >= damageTime)
         {
             health.TakeDamage(damage);
             damageTime = Time.time + damageInterval;
-        }
+        }*/
+    }
+
+    private IEnumerator DisableDamage()
+    {
+        damageEnabled = false;
+        yield return new WaitForSeconds(damageTime);
+        damageEnabled = true;
     }
 }
